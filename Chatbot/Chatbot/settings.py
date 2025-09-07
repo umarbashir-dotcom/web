c97114808406ef4a5f6896215f637394808ceb6e
umarbashir-dotcom <umarbasheer449@gmail.com>
2025-09-06 23:51:30 -0700

    Host ('mychatbot-gnkt.onrender.com') is allowed

--- Chatbot/Chatbot/settings.py
+++ Chatbot/Chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY","fallback-secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://mychatbot-gnkt.onrender.com","http://127.0.0.1:8000/"]
 
 
 # Application definition
